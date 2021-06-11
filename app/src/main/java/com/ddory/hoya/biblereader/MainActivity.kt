9d15c9a65ce5ddb764d3ae8b215281e7f081074c
Jongho <tghbjhpark@gmail.com>
2021-06-11 11:46:56 +0900

    signin 이 되어 있는지를 체크하는 로직 추가

--- app/src/main/java/com/ddory/hoya/biblereader/MainActivity.kt
+++ app/src/main/java/com/ddory/hoya/biblereader/MainActivity.kt
@@ -3,13 +3,25 @@ package com.ddory.hoya.biblereader
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ddory.hoya.biblereader.ui.main.MainFragment
+import com.ddory.hoya.biblereader.ui.signin.SigninFragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
+
+        auth = Firebase.auth
+        if (auth.currentUser == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, SigninFragment.newInstance())
+                .commitNow()
+        } else {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
