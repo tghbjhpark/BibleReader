0f1a9368ea6286a4f0be6633fdc32fba6e35f7dc
Jongho <tghbjhpark@gmail.com>
2021-06-20 00:53:18 +0900

    viewbinding enable

--- app/src/main/java/com/ddory/hoya/biblereader/MainActivity.kt
+++ app/src/main/java/com/ddory/hoya/biblereader/MainActivity.kt
@@ -2,11 +2,13 @@ package com.ddory.hoya.biblereader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.ddory.hoya.biblereader.ui.main.MainFragment
 import com.ddory.hoya.biblereader.ui.signin.SignInFragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +24,7 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, SignInFragment.newInstance())
                 .commitNow()
         } else {
+            bottom_navigation.visibility = View.VISIBLE
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
