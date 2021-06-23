8b0290ff8bf6fe4f676773b53c7cee086d3cc13b
Jongho <tghbjhpark@gmail.com>
2021-06-24 00:20:01 +0900

    remove unnessasery code

--- app/src/main/java/com/ddory/hoya/biblereader/MainActivity.kt
+++ app/src/main/java/com/ddory/hoya/biblereader/MainActivity.kt
@@ -3,36 +3,14 @@ package com.ddory.hoya.biblereader
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.navigation.NavDirections
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import com.ddory.hoya.biblereader.ui.main.MainFragment
-import com.ddory.hoya.biblereader.ui.signin.SignInFragment
-import com.ddory.hoya.biblereader.ui.splash.SplashFragmentDirections
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-
-//        auth = Firebase.auth
-//        if (auth.currentUser == null) {
-//            supportFragmentManager.beginTransaction()
-//                .replace(R.id.container, SignInFragment.newInstance())
-//                .commitNow()
-//        } else {
-//            bottom_navigation.visibility = View.VISIBLE
-//            supportFragmentManager.beginTransaction()
-//                .replace(R.id.container, MainFragment.newInstance())
-//                .commitNow()
-//        }
     }
 
     fun enableBottomNavigation(isEnable: Boolean) {
