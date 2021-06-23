87e7de4ca660e49f9d5c26a029638e4d89d55f96
Jongho <tghbjhpark@gmail.com>
2021-06-24 00:18:12 +0900

    Apply navigation

--- app/src/main/java/com/ddory/hoya/biblereader/MainActivity.kt
+++ app/src/main/java/com/ddory/hoya/biblereader/MainActivity.kt
@@ -3,8 +3,12 @@ package com.ddory.hoya.biblereader
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.NavDirections
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.ddory.hoya.biblereader.ui.main.MainFragment
 import com.ddory.hoya.biblereader.ui.signin.SignInFragment
+import com.ddory.hoya.biblereader.ui.splash.SplashFragmentDirections
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -18,16 +22,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
-        auth = Firebase.auth
-        if (auth.currentUser == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, SignInFragment.newInstance())
-                .commitNow()
-        } else {
-            bottom_navigation.visibility = View.VISIBLE
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+//        auth = Firebase.auth
+//        if (auth.currentUser == null) {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.container, SignInFragment.newInstance())
+//                .commitNow()
+//        } else {
+//            bottom_navigation.visibility = View.VISIBLE
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.container, MainFragment.newInstance())
+//                .commitNow()
+//        }
+    }
+
+    fun enableBottomNavigation(isEnable: Boolean) {
+        bottom_navigation.visibility = if (isEnable) View.VISIBLE else View.GONE
     }
 }
--- app/src/main/java/com/ddory/hoya/biblereader/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/ddory/hoya/biblereader/ui/splash/SplashFragment.kt
@@ -6,10 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.ddory.hoya.biblereader.MainActivity
 import com.ddory.hoya.biblereader.ViewModelFactory
 import com.ddory.hoya.biblereader.databinding.SplashFragmentBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.main_activity.*
 
 class SplashFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
 
     lateinit var binding: SplashFragmentBinding
 
@@ -20,8 +28,20 @@ class SplashFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = SplashFragmentBinding.inflate(inflater, container, false).apply {
             viewModel = splashViewModel
-            lifecycleOwner =viewLifecycleOwner
+            lifecycleOwner = viewLifecycleOwner
         }
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        auth = Firebase.auth
+        if (auth.currentUser == null) {
+            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToSignInFragment())
+
+        } else {
+            (activity as MainActivity).enableBottomNavigation(true)
+            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToHomeFragment())
+        }
     }
 }
