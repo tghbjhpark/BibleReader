fef034ee66e5899e0901ebc05b2f3c84ad1b8957
Jongho <tghbjhpark@gmail.com>
2023-05-27 23:52:57 +0900

    Add splash screen

--- app/src/main/java/com/ddory/hoya/biblereader/MainActivity.kt
+++ app/src/main/java/com/ddory/hoya/biblereader/MainActivity.kt
@@ -5,19 +5,39 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.ddory.hoya.biblereader.ui.home.HomeScreen
 import com.ddory.hoya.biblereader.ui.main.MainApp
 import com.ddory.hoya.biblereader.ui.splash.SplashScreen
 import com.ddory.hoya.biblereader.ui.splash.SplashViewModel
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     val splashViewModel by viewModels<SplashViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+        var uiState: Boolean by mutableStateOf(true)
+
+        lifecycleScope.launch {
+            delay(3000L)
+            uiState = false
+        }
+        splashScreen.setKeepOnScreenCondition {
+            uiState
+        }
         setContent {
-            SplashActivityScreen()
+//            SplashActivityScreen()
 //            MainApp()
+            HomeScreen()
         }
     }
 }
